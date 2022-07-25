7a5063f650e903dbfd8dace955d5924d6fe9548d
gruticyp <gruticyp@gmail.com>
2022-07-25 06:23:38 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        height: '150vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
